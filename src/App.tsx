a6a814acf6a2c6316e4874951652aa7370cddc46
DongYeon Kang <myjjue00@gmail.com>
2024-07-16 14:57:15 +0900

    feat: 회원가입 화면 구현
    - JoinPage 생성
    - App.tsx 라우팅 기능 추가 및 폴더 구조 재조정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import JoinPage from './components/pages/JoinPage/JoinPage';
 import './App.css';
 
-function App() {
-  return <div> 안녕하세요 </div>;
-}
+const App: React.FC = () => (
+  <Router>
+    <div className="app-container">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/join">Join</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/join" element={<JoinPage />} />
+        <Route path="/" element={<div>메인페이지</div>} />
+      </Routes>
+    </div>
+  </Router>
+);
 
 export default App;
