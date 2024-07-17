8d37a9a27cf7d9209b8a76e108ed96c5d7530468
CodeDiary18 <codediary18@gmail.com>
2024-07-17 23:36:15 +0900

    chore: 상태 관리 라이브러리 zustand로 대체
    TIL-73

--- src/store/rootReducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import exampleReducer from './exampleSlice';
-
-const rootReducer = combineReducers({
-  example: exampleReducer,
-});
-
-export default rootReducer;
