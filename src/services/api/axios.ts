32ad6fc2853dc94eb473577632c1d0c6c7781037
CodeDiary18 <codediary18@gmail.com>
2024-07-21 16:54:31 +0900

    chore: 환경변수 파일(.env) 사용하도록 설정

--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8080', // 기본 URL 설정
+  baseURL: process.env.REACT_APP_TIL_API_URL, // 기본 URL 설정
   headers: {
     'Content-Type': 'application/json',
   },
