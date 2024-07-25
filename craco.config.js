57df418bbdcf724d84b8d20df33d4b70fd8014c2
CodeDiary18 <codediary18@gmail.com>
2024-07-25 11:29:49 +0900

    chore: constants 디렉토리 절대경로 추가

--- craco.config.js
+++ craco.config.js
@@ -6,6 +6,7 @@ module.exports = {
   webpack: {
     alias: {
       '@components': path.resolve(__dirname, 'src/components/'),
+      '@constants': path.resolve(__dirname, 'src/constants/'),
       '@services': path.resolve(__dirname, 'src/services/'),
       '@store': path.resolve(__dirname, 'src/store/'),
       '@type': path.resolve(__dirname, 'src/type/'),
