120a084259b4275b8e1fa2da3928ad1ce186f711
CodeDiary18 <codediary18@gmail.com>
2024-07-29 15:08:21 +0900

    feat: 환경 변수로 인가 옵션 on/off 설정
    - 인증 비활성 모드시 url로 접근할 때 인증을 거치지 않고 접근할 수 있도록 설정

--- src/route.tsx
+++ src/route.tsx
@@ -7,6 +7,14 @@ interface PrivateRouteProps {
 
 const PrivateRoute = ({ authentication = true }: PrivateRouteProps): React.ReactElement | null => {
   const { isAuthenticated } = useAuthStore();
+
+  if (
+    process.env.REACT_APP_PROFILE !== 'prod' &&
+    process.env.REACT_APP_AUTH_MODE_DISABLED === 'true'
+  ) {
+    return <Outlet />;
+  }
+
   if (authentication) {
     return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
   }
