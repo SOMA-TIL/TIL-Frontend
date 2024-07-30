dab52066b077395a0bdf56e3949f8d794f9c9da5
CodeDiary18 <codediary18@gmail.com>
2024-07-31 00:47:48 +0900

    refactor: 메인 화면 배너 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import PageLayout from '@components/layout/PageLayout';
 
+import MainPage from '@components/pages/MainPage/MainPage';
 import JoinPage from '@components/pages/JoinPage/JoinPage';
 import LoginPage from '@components/pages/LoginPage/LoginPage';
 import InterviewIntroPage from '@components/pages/InterviewIntroPage/InterviewIntroPage';
@@ -13,7 +14,7 @@ const App: React.FC = () => (
   <PageLayout>
     <Routes>
       {/* 인증 여부 상관 없이 접속 가능한 페이지 정의 */}
-      <Route path="/" element={<div>메인페이지</div>} />
+      <Route path="/" element={<MainPage />} />
       <Route path="/problem" element={<div>기술학습</div>} />
 
       {/* 인증이 필요한 페이지 정의 */}
