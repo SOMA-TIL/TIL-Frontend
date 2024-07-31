1a3aecd88a3a5b0aabcb23e176dcda8dfa863999
CodeDiary18 <codediary18@gmail.com>
2024-07-31 21:59:18 +0900

    chore: styled-component 관련 설정 추가

--- craco.config.js
+++ craco.config.js
@@ -9,6 +9,7 @@ module.exports = {
       '@constants': path.resolve(__dirname, 'src/constants/'),
       '@services': path.resolve(__dirname, 'src/services/'),
       '@store': path.resolve(__dirname, 'src/store/'),
+      '@styles': path.resolve(__dirname, 'src/styles/'),
       '@type': path.resolve(__dirname, 'src/type/'),
       '@utils': path.resolve(__dirname, 'src/utils/'),
     },
