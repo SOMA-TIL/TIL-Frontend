0375390b2a8b00b03d35e0c0d4edee63326a4f6b
CodeDiary18 <codediary18@gmail.com>
2024-08-01 17:18:04 +0900

    refactor: header 스타일링 적용

--- src/components/layout/Header/HeaderMenuItem.tsx
+++ src/components/layout/Header/HeaderMenuItem.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 
-import { MenuItem } from './Header.style';
+import { MenuItem, LogoutButton } from './Header.style';
 
 interface HeaderMenuButtonProps {
   title: string;
@@ -19,6 +19,23 @@ const HeaderMenuItem: React.FC<HeaderMenuButtonProps> = ({ title, path, onClick 
   };
 
   return <MenuItem onClick={handleHeaderMenuButton}>{title}</MenuItem>;
+};
+
+export const HeaderLogoutButton: React.FC<HeaderMenuButtonProps> = ({ title, path, onClick }) => {
+  const navigate = useNavigate();
+
+  const handleButton = () => {
+    if (onClick) {
+      return onClick();
+    }
+    return navigate(path);
+  };
+
+  return (
+    <LogoutButton onClick={handleButton}>
+      <img src="images/logout_icon.png" alt="logout" /> {title}
+    </LogoutButton>
+  );
 };
 
 export default HeaderMenuItem;
