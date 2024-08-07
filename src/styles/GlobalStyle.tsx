633d84e5df78fc058b371fb09002f2abcd6cb6e8
CodeDiary18 <codediary18@gmail.com>
2024-08-07 16:36:08 +0900

    chore: Pretendard 폰트 설정

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  @import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css");
+  
   * {
     margin: 0;
     padding: 0;
@@ -12,6 +14,8 @@ const GlobalStyle = createGlobalStyle`
     flex-direction: column;
     min-height: 100vh;
     min-width: 1400px;
+    font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
+    font-size: 16px;
   }
 
   button {
