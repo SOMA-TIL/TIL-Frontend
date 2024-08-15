1939e09d391a857564f8636a49f9b204b0c52ef0
DongYeon Kang <myjjue00@gmail.com>
2024-08-15 19:00:26 +0900

    feat: 검색 바 기능 구현 & 스타일링
    - 검색 바에 유효한 값 Get하고 선택 기능
    - 스타일링 작업

--- src/components/pages/ProblemListPage/ProblemListPage.style.tsx
+++ src/components/pages/ProblemListPage/ProblemListPage.style.tsx
@@ -6,9 +6,8 @@ export const ProblemPageContainer = styled.div`
   padding: 20px 200px;
 `;
 
-export const SubTitle = styled.h4`
-  height: 40px;
-  padding: 10px;
+export const SubTitle = styled.h3`
+  padding: 5px;
 `;
 
 export const Table = styled.div`
