526161c3375441f7e342ecce7e4279cb4c068467
DongYeon Kang <myjjue00@gmail.com>
2024-08-17 07:00:27 +0900

    refactor: 기술 학습 상세 화면 스타일링

--- src/components/pages/ProblemDetailPage/ProblemDetailPage.tsx
+++ src/components/pages/ProblemDetailPage/ProblemDetailPage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 
-import { Tabs, Modal } from 'antd';
+import { Modal } from 'antd';
 import Loading from '@components/common/loading/Loading';
 import { useToast } from '@components/common/notification/ToastProvider';
 import BasicPageLayout from '@components/layout/BasicPageLayout';
@@ -22,6 +22,12 @@ import useAuthStore from '@store/useAuthStore';
 import useLoadingStore from '@store/useLoadingStore';
 import useCategoryStore from '@store/useCategoryStore';
 import {
+  QuestionCircleOutlined,
+  MessageOutlined,
+  RedoOutlined,
+  FileDoneOutlined,
+} from '@ant-design/icons';
+import {
   ProblemDetailContainer,
   ProblemInfoBar,
   ProblemInfo,
@@ -33,15 +39,17 @@ import {
   QuestionSection,
   AnswerSection,
   CustomButton,
-  CustomLoginButton,
-  StyledTextArea,
   TextDiv,
   ButtonGroup,
   BookMarkIcon,
+  InfoGroup,
+  QuestionTitle,
+  TabMenuItem,
+  TabMenu,
+  LevelText,
+  ProblemSolveFooterButton,
 } from './ProblemDetailPage.style';
 import ProblemHistory from './ProblemHistoryTab';
-
-const { TabPane } = Tabs;
 
 const ProblemDetailPage: React.FC = () => {
   const { notify } = useToast();
@@ -53,6 +61,7 @@ const ProblemDetailPage: React.FC = () => {
   const [answer, setAnswer] = useState<string>('');
   const [result, setResult] = useState<string | null>(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [activeTab, setActiveTab] = useState('answer');
   const { isAuthenticated, checkAuthentication } = useAuthStore();
   const { getIsLoading, setIsLoading } = useLoadingStore();
   const { getCategoryList, transformCategoryTagList } = useCategoryStore();
@@ -162,63 +171,61 @@ const ProblemDetailPage: React.FC = () => {
 
   return (
     <BasicPageLayout>
-      {/* todo: 사이드바 사이즈 조절 가능허도록 수정 예정, 내 답변 기록 데이터 API 연동 필요 */}
       <ProblemDetailContainer>
         <ProblemInfoBar>
-          <BookMarkIcon
-            isFavorite={problemDetail?.isFavorite ?? false}
-            onClick={handleClickFavorite}
-          />
-          <Title>{problemDetail && problemDetail.title}</Title>
-          <Category>
-            {Array.isArray(problemDetail.categoryList)
-              ? transformCategoryTagList(problemDetail.categoryList).join(',')
-              : 'None'}
-          </Category>
-          <ProblemInfo>난이도: {problemDetail?.level}</ProblemInfo>
-          <ProblemInfo>완료한 사람: 0명</ProblemInfo>
-          <ProblemInfo>정답률: 0%</ProblemInfo>
+          <InfoGroup>
+            <BookMarkIcon
+              isFavorite={problemDetail?.isFavorite ?? false}
+              onClick={handleClickFavorite}
+            />
+            <Title>{problemDetail && problemDetail.title}</Title>
+            <Category>
+              {Array.isArray(problemDetail.categoryList)
+                ? transformCategoryTagList(problemDetail.categoryList).join(',')
+                : 'None'}
+            </Category>
+          </InfoGroup>
+          <InfoGroup>
+            <ProblemInfo>
+              난이도
+              <LevelText>Lv.{problemDetail?.level}</LevelText>
+            </ProblemInfo>
+            <ProblemInfo>완료한 사람 0명</ProblemInfo>
+            <ProblemInfo>정답률 0%</ProblemInfo>
+          </InfoGroup>
         </ProblemInfoBar>
         <ContentContainer>
           <QuestionSection>
-            <h3>문제 설명</h3>
+            <QuestionTitle>문제 설명</QuestionTitle>
             <Question>{problemDetail?.question}</Question>
           </QuestionSection>
           <AnswerSection>
-            <Tabs defaultActiveKey="1" type="card">
-              <TabPane tab="답변" key="1">
-                <TextDiv>
-                  <StyledTextArea
-                    value={answer}
-                    onChange={(e) => setAnswer(e.target.value)}
-                    rows={25}
-                    placeholder="답변을 입력하세요."
-                    disabled={!isAuthenticated}
-                  />
-                </TextDiv>
-              </TabPane>
-              <TabPane tab="내 답변 기록" key="2" disabled={!isAuthenticated}>
-                {id && <ProblemHistory problemId={id} />}
-              </TabPane>
-            </Tabs>
+            <TabMenu>
+              <TabMenuItem active={activeTab === 'answer'} onClick={() => setActiveTab('answer')}>
+                답변
+              </TabMenuItem>
+              {isAuthenticated && (
+                <TabMenuItem
+                  active={activeTab === 'history'}
+                  onClick={() => setActiveTab('history')}
+                >
+                  내 답변 기록
+                </TabMenuItem>
+              )}
+            </TabMenu>
+            {activeTab === 'answer' && (
+              <TextDiv
+                value={answer}
+                onChange={(e) => setAnswer(e.target.value)}
+                placeholder={
+                  isAuthenticated ? '답변을 입력하세요.' : '로그인 후, 답변을 입력하세요.'
+                }
+                disabled={!isAuthenticated}
+              />
+            )}
+            {activeTab === 'history' && id && <ProblemHistory problemId={id} />}
           </AnswerSection>
         </ContentContainer>
-        <BottomBar>
-          {!isAuthenticated ? (
-            <CustomLoginButton onClick={handleLoginRedirect}>로그인하기</CustomLoginButton>
-          ) : (
-            <>
-              <ButtonGroup>
-                <CustomButton>질문하기</CustomButton>
-                <CustomButton>다른 사람의 답변</CustomButton>
-              </ButtonGroup>
-              <ButtonGroup>
-                <CustomButton onClick={handleReset}>답변 초기화</CustomButton>
-                <CustomButton onClick={handleSubmit}>제출</CustomButton>
-              </ButtonGroup>
-            </>
-          )}
-        </BottomBar>
         <Modal
           title="채점 결과"
           open={isModalVisible}
@@ -232,6 +239,36 @@ const ProblemDetailPage: React.FC = () => {
           <p>{result}</p>
         </Modal>
       </ProblemDetailContainer>
+      <BottomBar>
+        {!isAuthenticated ? (
+          <ProblemSolveFooterButton type="PURPLE" onClick={handleLoginRedirect}>
+            로그인하기
+          </ProblemSolveFooterButton>
+        ) : (
+          <>
+            <ButtonGroup>
+              <ProblemSolveFooterButton type="">
+                <QuestionCircleOutlined />
+                질문하기
+              </ProblemSolveFooterButton>
+              <ProblemSolveFooterButton type="">
+                <MessageOutlined />
+                다른 사람의 답변
+              </ProblemSolveFooterButton>
+            </ButtonGroup>
+            <ButtonGroup>
+              <ProblemSolveFooterButton type="RED" onClick={handleReset}>
+                <RedoOutlined />
+                답변 초기화
+              </ProblemSolveFooterButton>
+              <ProblemSolveFooterButton type="PURPLE" onClick={handleSubmit}>
+                <FileDoneOutlined />
+                답변 제출
+              </ProblemSolveFooterButton>
+            </ButtonGroup>
+          </>
+        )}
+      </BottomBar>
     </BasicPageLayout>
   );
 };
