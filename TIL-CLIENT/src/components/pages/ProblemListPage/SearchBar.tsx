067224d413f61855b53feec819498ceedabd672f
DongYeon Kang <myjjue00@gmail.com>
2024-08-29 06:34:55 +0900

    feat: 정렬 기능 추가

--- TIL-CLIENT/src/components/pages/ProblemListPage/SearchBar.tsx
+++ TIL-CLIENT/src/components/pages/ProblemListPage/SearchBar.tsx
@@ -4,7 +4,6 @@ import { useNavigate } from 'react-router-dom';
 import useAuthStore from '@store/useAuthStore';
 import { Category } from '@type/category';
 import { SearchOutlined, ReloadOutlined } from '@ant-design/icons';
-import StyledConfigProvider from '@components/common/ant/AntStyleProvider';
 import { PROBLEM_LEVEL, PROBLEM_USER_STATUS } from '@constants/problem';
 import { ProblemListParams } from '@services/api/problemService';
 import {
@@ -85,56 +84,54 @@ const SearchBar: React.FC<SearchBarProps> = ({ categoryList, initSearchParam }) 
   };
 
   return (
-    <StyledConfigProvider>
-      <SearchBarContainer>
-        <SearchInput
-          placeholder="풀고 싶은 문제의 제목을 검색하세요."
-          value={keyword}
-          onChange={(e) => setKeyword(e.target.value)}
-          prefix={<SearchOutlined />}
-        />
-        {isAuthenticated && (
-          <StatusSelect value={status} onChange={handleStatusChange} placeholder="상태 선택">
-            <Option value="">전체</Option>
-            <Option value={PROBLEM_USER_STATUS.PASS}>PASS</Option>
-            <Option value={PROBLEM_USER_STATUS.FAIL}>FAIL</Option>
-            <Option value={PROBLEM_USER_STATUS.NOT_ATTEMPTED}>시도하지 않은 문제</Option>
-          </StatusSelect>
-        )}
-        <LevelSelect
-          mode="multiple"
-          value={selectedLevel?.sort((a, b) => a - b)}
-          onChange={handleLevelChange}
-          placeholder="난이도 선택"
-          hasStatus={isAuthenticated}
-        >
-          {Object.values(PROBLEM_LEVEL).map((level) => (
-            <Option key={level} value={level}>
-              Lv.{level}
-            </Option>
-          ))}
-        </LevelSelect>
-        <CategorySelect
-          mode="multiple"
-          value={selectedCategoryList}
-          onChange={handleCategoryChange}
-          placeholder="기술 카테고리 선택"
-        >
-          {categoryList.map((category) => (
-            <Option key={category.id} value={category.id}>
-              {category.tag}
-            </Option>
-          ))}
-        </CategorySelect>
-        <SearchButton onClick={handleSearch}>
-          검색하기
-          <SearchOutlined className="icon" />
-        </SearchButton>
-        <ResetButton onClick={handleReset}>
-          <ReloadOutlined />
-        </ResetButton>
-      </SearchBarContainer>
-    </StyledConfigProvider>
+    <SearchBarContainer>
+      <SearchInput
+        placeholder="풀고 싶은 문제의 제목을 검색하세요."
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+        prefix={<SearchOutlined />}
+      />
+      {isAuthenticated && (
+        <StatusSelect value={status} onChange={handleStatusChange} placeholder="상태 선택">
+          <Option value="">전체</Option>
+          <Option value={PROBLEM_USER_STATUS.PASS}>PASS</Option>
+          <Option value={PROBLEM_USER_STATUS.FAIL}>FAIL</Option>
+          <Option value={PROBLEM_USER_STATUS.NOT_ATTEMPTED}>시도하지 않은 문제</Option>
+        </StatusSelect>
+      )}
+      <LevelSelect
+        mode="multiple"
+        value={selectedLevel?.sort((a, b) => a - b)}
+        onChange={handleLevelChange}
+        placeholder="난이도 선택"
+        hasStatus={isAuthenticated}
+      >
+        {Object.values(PROBLEM_LEVEL).map((level) => (
+          <Option key={level} value={level}>
+            Lv.{level}
+          </Option>
+        ))}
+      </LevelSelect>
+      <CategorySelect
+        mode="multiple"
+        value={selectedCategoryList}
+        onChange={handleCategoryChange}
+        placeholder="기술 카테고리 선택"
+      >
+        {categoryList.map((category) => (
+          <Option key={category.id} value={category.id}>
+            {category.tag}
+          </Option>
+        ))}
+      </CategorySelect>
+      <SearchButton onClick={handleSearch}>
+        검색하기
+        <SearchOutlined className="icon" />
+      </SearchButton>
+      <ResetButton onClick={handleReset}>
+        <ReloadOutlined />
+      </ResetButton>
+    </SearchBarContainer>
   );
 };
 
